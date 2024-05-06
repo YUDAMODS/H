49eb171764f9bd12a7d58e63b0e25f7cc4cb74e4
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-05-06 17:58:41 +0700

    Add files via upload

--- YudaMods-Wa-Vvip/XeonCheems10.js
+++ YudaMods-Wa-Vvip/XeonCheems10.js
@@ -563,30 +563,6 @@ title: botname
 }
 }
 
-const fakeflow = {
-key: {
-remoteJid: 'status@broadcast',
-fromMe: false,
-participant: '0@s.whatsapp.net'
-},
-'message': {
-"interactiveMessage": {
-"header": {
-"hasMediaAttachment": true,
-"jpegThumbnail": bimg
-},
-"nativeFlowMessage": {
-"buttons": [
-{
-"name": "review_and_pay",
-"buttonParamsJson": `{\"currency\":\"INR\",\"payment_configuration\":\"\",\"payment_type\":\"\",\"total_amount\":{\"value\":45416739902,\"offset\":100},\"reference_id\":\"40NRXY65HS6\",\"type\":\"physical-goods\",\"order\":{\"status\":\"preparing_to_ship\",\"description\":\"\",\"subtotal\":{\"value\":29759959800,\"offset\":100},\"tax\":{\"value\":14805580001,\"offset\":100},\"discount\":{\"value\":148799799,\"offset\":100},\"shipping\":{\"value\":99999999900,\"offset\":100},\"order_type\":\"ORDER\",\"items\":[{\"retailer_id\":\"7537631592926009\",\"product_id\":\"7538731592926009\",\"name\":\"${botname}\",\"amount\":{\"value\":9999900,\"offset\":100},\"quantity\":999999},{\"retailer_id\":\"7842674605763435\",\"product_id\":\"7842674605763435\",\"name\":\"${ownername}\",\"amount\":{\"value\":9999900,\"offset\":100},\"quantity\":999999},{\"retailer_id\":\"custom-item-2465f31c-4f49-46eb-9ad0-5ec5121a9c0d\",\"name\":\"\",\"amount\":{\"value\":99999900,\"offset\":100},\"quantity\":99},{\"retailer_id\":\"custom-item-13746d0a-55f0-4942-86b2-2094bc9a2978\",\"name\":\"\",\"amount\":{\"value\":99999800,\"offset\":100},\"quantity\":999999},{\"retailer_id\":\"custom-item-ab993749-045b-4f66-a1d3-264fe18fb1d0\",\"name\":\"\",\"amount\":{\"value\":99999900,\"offset\":100},\"quantity\":999999}]},\"additional_note\":\"\",\"native_payment_methods\":[]}`
-}
-]
-}
-}
-}
-};
-
 const xeonimun = (texto) => {
 XeonBotInc.sendMessage(from, { text: texto, mentions: [sender]}, {quoted: m }).catch(e => {
 return reply("Erro..")
